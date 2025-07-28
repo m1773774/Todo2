0dc10d1017be41941cab65179b185dd0ccd77807
m1773774 <m1773774@gmail.com>
2025-07-28 15:33:10 +0800

    Cool

--- Todo.sol
+++ Todo.sol
@@ -1,13 +1,32 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.26;
 
-contract MyTodo{
+contract MyTodo {
+    struct Todo {
+        string text;
+        bool completed;
+    }
 
-struct Todo{
-    string text;
-    bool completed;
-}
+    Todo[] public todos;
 
+    function create(string calldata _text) public {
+        todos.push(Todo(_text, false));
 
+        todos.push(Todo({text: _text, completed: false}));
+
+        Todo memory todo;
+        todo.text = _text;
+        todo.completed = false;
+        //todos.push(todo);
+    }
+    
+    function update(uint _index, string calldata _text) public {
+        Todo storage todo = todos[_index];
+        todo.text = _text;
+    }
+
+    function delTodo(uint _index) public {
+        delete todos[_index];
+    }
 
 }
